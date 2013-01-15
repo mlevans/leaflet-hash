825d578bef01ef48842382aaed65d3a1f74a357b
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-01-15 08:39:45 -0500

    major rewrite

---
+++ leaflet-hash.coffee
@@ -0,0 +1,77 @@
+class Hash
+	constructor: (@map) ->
+		if history.pushState
+			@withPushState()
+		else
+			@withoutPushState()
+	withPushState : ->
+		window.onpopstate=(event)=>
+			if event.state
+				@map.setView event.state.center, event.state.zoom
+		if location.hash
+			parsed = @parseHash location.hash
+			@map.setView parsed.center, parsed.zoom
+		else
+			history.replaceState @formatHash()...
+		@map.on "moveend", ()=>
+			pstate = @formatHash()
+			if location.hash != pstate[2]
+				history.pushState pstate...
+	withoutPushState : ->
+		if location.hash
+			parsed = @parseHash location.hash
+			@map.setView parsed.center, parsed.zoom
+		else
+			location.hash = @formatHash()[2]
+		@map.on "moveend", ()=>
+			pstate = @formatHash()
+			if location.hash != pstate[2]
+				location.hash = pstate[2]
+		if ('onhashchange' of window) and (window.documentMode == undefined or window.documentMode > 7)
+			window.onhashchange = ()=>
+				if location.hash
+					parsed = @parseHash location.hash
+					@map.setView parsed.center, parsed.zoom
+		else
+			onHashChange = ()=>
+				pstate = @formatHash()
+				if location.hash != pstate[2]
+					location.hash = pstate[2]
+			@hashChangeInterval = setInterval onHashChange, 50
+
+	parseHash : (hash) ->
+		if hash.indexOf('#') == 0
+			hash = hash.substr(1);
+		args = hash.split("/");
+		if (args.length == 3) 
+			zoom = parseInt(args[0], 10)
+			lat = parseFloat(args[1])
+			lon = parseFloat(args[2])
+			if  isNaN(zoom) or isNaN(lat) or isNaN(lon)
+				return false;
+			else
+				return {
+					center: new L.LatLng(lat, lon)
+					zoom: zoom
+				}
+		else
+			return false;
+    
+	formatHash : () =>
+		center = @map.getCenter()
+		zoom = @map.getZoom()
+		precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2))
+		[{center:center,zoom:zoom},"a",'#' + [zoom,center.lat.toFixed(precision),center.lng.toFixed(precision)].join("/")]
+	
+	remove : ()=>
+		@map.off "moveend"
+		if window.onpopstate
+			window.onpopstate = null
+		location.hash=""
+L.Map::addHash=()->
+	@_hash =  new Hash(@)
+	@
+
+L.Map::removeHash=()->
+	@_hash.remove()
+	@
--- leaflet-hash.js
+++ leaflet-hash.js
@@ -1,237 +1,136 @@
-(function(window) {
+(function() {
+  var Hash,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-	L.Hash = function(map) {
-		this.onHashChange = L.Util.bind(this.onHashChange, this);
-		this.HAS_HASHCHANGE = (function() {
-			var doc_mode = window.documentMode;
-			return ('onhashchange' in window) &&
-				(doc_mode === undefined || doc_mode > 7);
-		})();
-		if (map) {
-			this.init(map);
-		}
-	};
-	
-	L.Hash.prototype = {
-		map: null,
-		lastHash: null,
-	
-		parseHash: function(hash) {
-			if(hash.indexOf('#') == 0) {
-				hash = hash.substr(1);
-			}
-			var args = hash.split("/");
-			if (args.length == 3) {
-				var zoom = parseInt(args[0], 10),
-					lat = parseFloat(args[1]),
-					lon = parseFloat(args[2]);
-				if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
-					return false;
-				} else {
-					return {
-						center: new L.LatLng(lat, lon),
-						zoom: zoom
-					};
-				}
-			} else {
-				return false;
-			}
-		},
-	
-		formatHash: function(map) {
-			var center = map.getCenter(),
-				zoom = map.getZoom(),
-				precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
-			
-			return "#" + [zoom,
-				center.lat.toFixed(precision),
-				center.lng.toFixed(precision)
-			].join("/");
-		},
-	
-		init: function(map) {
-			this.map = map;
-			
-			this.map.on("moveend", this.onMapMove, this);
-			
-			// reset the hash
-			this.lastHash = null;
-			this.onHashChange();
-	
-			if (!this.isListening) {
-				this.startListening();
-			}
-		},
-	
-		remove: function() {
-			this.map = null;
-			if (this.isListening) {
-				this.stopListening();
-			}
-		},
-		
-		onMapMove: function(map) {
-			// bail if we're moving the map (updating from a hash),
-			// or if the map has no zoom set
-			
-			if (this.movingMap || this.map.getZoom() === 0) {
-				return false;
-			}
-			
-			var hash = this.formatHash(this.map);
-			if (this.lastHash != hash) {
-				location.replace(hash);
-				this.lastHash = hash;
-			}
-		},
-	
-		movingMap: false,
-		update: function() {
-			var hash = location.hash;
-			if (hash === this.lastHash) {
-				// console.info("(no change)");
-				return;
-			}
-			var parsed = this.parseHash(hash);
-			if (parsed) {
-				// console.log("parsed:", parsed.zoom, parsed.center.toString());
-				this.movingMap = true;
-				
-				this.map.setView(parsed.center, parsed.zoom);
-				
-				this.movingMap = false;
-			} else {
-				// console.warn("parse error; resetting:", this.map.getCenter(), this.map.getZoom());
-				this.onMapMove(this.map);
-			}
-		},
-	
-		// defer hash change updates every 100ms
-		changeDefer: 100,
-		changeTimeout: null,
-		onHashChange: function() {
-			// throttle calls to update() so that they only happen every
-			// `changeDefer` ms
-			if (!this.changeTimeout) {
-				var that = this;
-				this.changeTimeout = setTimeout(function() {
-					that.update();
-					that.changeTimeout = null;
-				}, this.changeDefer);
-			}
-		},
-	
-		isListening: false,
-		hashChangeInterval: null,
-		startListening: function() {
-			if (this.HAS_HASHCHANGE) {
-				L.DomEvent.addListener(window, "hashchange", this.onHashChange);
-			} else {
-				clearInterval(this.hashChangeInterval);
-				this.hashChangeInterval = setInterval(this.onHashChange, 50);
-			}
-			this.isListening = true;
-		},
-	
-		stopListening: function() {
-			if (this.HAS_HASHCHANGE) {
-				L.DomEvent.removeListener(window, "hashchange", this.onHashChange);
-			} else {
-				clearInterval(this.hashChangeInterval);
-			}
-			this.isListening = false;
-		}
-	};
-	var StatefulHash,
-  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  Hash = (function() {
 
-StatefulHash = (function() {
+    function Hash(map) {
+      this.map = map;
+      this.remove = __bind(this.remove, this);
 
-  function StatefulHash(map) {
-    var parsed,
-      _this = this;
-    this.map = map;
-    this.remove = __bind(this.remove, this);
+      this.formatHash = __bind(this.formatHash, this);
 
-    this.formatHash = __bind(this.formatHash, this);
+      if (history.pushState) {
+        this.withPushState();
+      } else {
+        this.withoutPushState();
+      }
+    }
 
-    window.onpopstate = function(event) {
-      if (event.state) {
-        return _this.map.setView(event.state.center, event.state.zoom);
+    Hash.prototype.withPushState = function() {
+      var parsed,
+        _this = this;
+      window.onpopstate = function(event) {
+        if (event.state) {
+          return _this.map.setView(event.state.center, event.state.zoom);
+        }
+      };
+      if (location.hash) {
+        parsed = this.parseHash(location.hash);
+        this.map.setView(parsed.center, parsed.zoom);
+      } else {
+        history.replaceState.apply(history, this.formatHash());
+      }
+      return this.map.on("moveend", function() {
+        var pstate;
+        pstate = _this.formatHash();
+        if (location.hash !== pstate[2]) {
+          return history.pushState.apply(history, pstate);
+        }
+      });
+    };
+
+    Hash.prototype.withoutPushState = function() {
+      var onHashChange, parsed,
+        _this = this;
+      if (location.hash) {
+        parsed = this.parseHash(location.hash);
+        this.map.setView(parsed.center, parsed.zoom);
+      } else {
+        location.hash = this.formatHash()[2];
+      }
+      this.map.on("moveend", function() {
+        var pstate;
+        pstate = _this.formatHash();
+        if (location.hash !== pstate[2]) {
+          return location.hash = pstate[2];
+        }
+      });
+      if (('onhashchange' in window) && (window.documentMode === void 0 || window.documentMode > 7)) {
+        return window.onhashchange = function() {
+          if (location.hash) {
+            parsed = _this.parseHash(location.hash);
+            return _this.map.setView(parsed.center, parsed.zoom);
+          }
+        };
+      } else {
+        onHashChange = function() {
+          var pstate;
+          pstate = _this.formatHash();
+          if (location.hash !== pstate[2]) {
+            return location.hash = pstate[2];
+          }
+        };
+        return this.hashChangeInterval = setInterval(onHashChange, 50);
       }
     };
-    if (location.hash) {
-      console.log("hash is " + location.hash);
-      parsed = this.parseHash(location.hash);
-      this.map.setView(parsed.center, parsed.zoom);
-    } else {
-      history.replaceState.apply(history, this.formatHash());
-    }
-    this.map.on("moveend", function() {
-      var pstate;
-      pstate = _this.formatHash();
-      if (location.hash !== pstate[2]) {
-        return history.pushState.apply(history, pstate);
-      }
-    });
-  }
 
-  StatefulHash.prototype.parseHash = function(hash) {
-    var args, lat, lon, zoom;
-    if (hash.indexOf('#') === 0) {
-      hash = hash.substr(1);
-    }
-    args = hash.split("/");
-    if (args.length === 3) {
-      zoom = parseInt(args[0], 10);
-      lat = parseFloat(args[1]);
-      lon = parseFloat(args[2]);
-      if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
-        return false;
+    Hash.prototype.parseHash = function(hash) {
+      var args, lat, lon, zoom;
+      if (hash.indexOf('#') === 0) {
+        hash = hash.substr(1);
+      }
+      args = hash.split("/");
+      if (args.length === 3) {
+        zoom = parseInt(args[0], 10);
+        lat = parseFloat(args[1]);
+        lon = parseFloat(args[2]);
+        if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
+          return false;
+        } else {
+          return {
+            center: new L.LatLng(lat, lon),
+            zoom: zoom
+          };
+        }
       } else {
-        return {
-          center: new L.LatLng(lat, lon),
-          zoom: zoom
-        };
+        return false;
       }
-    } else {
-      return false;
-    }
+    };
+
+    Hash.prototype.formatHash = function() {
+      var center, precision, zoom;
+      center = this.map.getCenter();
+      zoom = this.map.getZoom();
+      precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
+      return [
+        {
+          center: center,
+          zoom: zoom
+        }, "a", '#' + [zoom, center.lat.toFixed(precision), center.lng.toFixed(precision)].join("/")
+      ];
+    };
+
+    Hash.prototype.remove = function() {
+      this.map.off("moveend");
+      if (window.onpopstate) {
+        window.onpopstate = null;
+      }
+      return location.hash = "";
+    };
+
+    return Hash;
+
+  })();
+
+  L.Map.prototype.addHash = function() {
+    this._hash = new Hash(this);
+    return this;
   };
 
-  StatefulHash.prototype.formatHash = function() {
-    var center, precision, zoom;
-    center = this.map.getCenter();
-    zoom = this.map.getZoom();
-    precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
-    return [
-      {
-        center: center,
-        zoom: zoom
-      }, "a", '#' + [zoom, center.lat.toFixed(precision), center.lng.toFixed(precision)].join("/")
-    ];
+  L.Map.prototype.removeHash = function() {
+    this._hash.remove();
+    return this;
   };
 
-  StatefulHash.prototype.remove = function() {
-    this.map.off("moveend");
-    window.onpopstate = null;
-    return location.hash = "";
-  };
-
-  return StatefulHash;
-
-})();
-	L.hash = function(map){
-		if(window.onpopstate){
-			return new StatefulHash(map)
-		}else{
-			return new L.Hash(map);	
-		}
-	};
-	L.Map.prototype.addHash = function(){
-		this._hash = L.hash(this);
-	};
-	L.Map.prototype.removeHash = function(){
-		this._hash.remove();
-	}
-})(window);
+}).call(this);
